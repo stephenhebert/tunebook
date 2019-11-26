83dd01b824b746c961b836a4f737e7591dbf05a5
Stephen Hebert <stephenjosephhebert@gmail.com>
2019-11-25 21:57:56 -0600

    added resources

--- js/tunes.js
+++ js/tunes.js
@@ -42,15 +42,12 @@ let tunes = [
             |:"Em"eB B2 efge|eB B2 gedB|"D"A2 FA DAFA|A2 FA defg|
             "Em"eB B2 eBgB|eB B2 defg|"D"afe^c dBAF|1"Em"DEFD E2 gf:|2"Em"DEFD E4|]
         `,
-        tags: ''
-    },
-    {
-        abc: 'X:1\nT: Cooley\'s\nM: 4/4\nL: 1/8\nR: reel\nK: Emin\nD2|:"Em"EB{c}BA B2 EB|~B2 AB dBAG|"D"FDAD BDAD|FDAD dAFD|\n"Em"EBBA B2 EB|B2 AB defg|"D"afe^c dBAF|1"Em"DEFD E2 D2:|2"Em"DEFD E2 gf||\n|:"Em"eB B2 efge|eB B2 gedB|"D"A2 FA DAFA|A2 FA defg|\n"Em"eB B2 eBgB|eB B2 defg|"D"afe^c dBAF|1"Em"DEFD E2 gf:|2"Em"DEFD E4|]\n',
-        tags: ''
-    },
-    {
-        abc: `X:6467\nT:Cold Frosty Morning\nM:4/4\nL:1/8\nR:reel\nK:Ador\nEDEGA2A2|ABcde2(3DEF|GABAG2D2|GABcd2D2|\nEDEGA2A2|ABcde2ed|cBAc BAGB|A4A4:|\nedega2a2|abag e2ef|gfga gfef|gagfe2e2|\nA2AAc2cc|d2dde2ee|cBAc BAGB|A4A4:|`,
-        tags: ['irish', 'bluegrass','reel']
+        tags: '',
+        links: [
+            {
+                title: "Backing Track (strum machine)", 
+                url: "https://strummachine.com/app/songs/7Gs8Kt2NxjzbnXgqP"
+            }]
     },
     {
         abc: `
@@ -60,27 +57,14 @@ let tunes = [
             Q:1/4=120
             M:3/4
             K:C
-            (3:2:2a2 g | (3:2:2a2 g (3:2:2e2 ^d e2 | a2 (3:2:2e2 a (3:2:2g2 e | (3age (3:2:1!slide!e3 (3edc | 
-            A2- (3:2:2A2 B (3:2:2c2 A | !slide!e2 !slide!e2 !slide!e2 | e2- (3:2:2e2 a (3:2:2g2 e | (3age (3:2:1e3 (3edc | A4 ] 
+            (3:2:2a2 g | "Am"(3:2:2a2 g (3:2:2e2 ^d e2 | a2 (3:2:2e2 a (3:2:2g2 e | (3age (3:2:1!slide!e3 (3edc | 
+            A2- (3:2:2A2 B (3:2:2c2 A | !slide!e2 !slide!e2 !slide!e2 | "E" e2- (3:2:2e2 a (3:2:2g2 e | (3age (3:2:1e3 (3edc | "Am" A4 ] 
         `,
-        tags: ["cajun","steve's favs"],
-        resources: [
-            {title: "Backing Track (strum machine)", 
-             link: "https://strummachine.com/app/songs/7Gs8Kt2NxjzbnXgqP"}]
-    },
-    {
-        abc: `
-            X:1
-            T: Cooley's Backing Track Test
-            M: 4/4
-            L: 1/8
-            R: reel
-            K: Emin
-            D2|:"Em"||"D"||
-            "Em"||"D"|1"Em":|2"Em"||
-            |:"Em"||"D"||
-            "Em"||"D"|1"Em":|2"Em"|]
-        `,
-        tags: ''
+        tags: ["cajun","waltz","steve's favs"],
+        links: [
+            {
+                title: "Valse de Balfa - Cajun Roosters (Spotify)",
+                url: "https://open.spotify.com/track/5m2OpNfZexMtJX3TtEW7oN?si=fwUtayH7QVSUM8PiJaoQ-w"
+            }]
     }
 ];
